2e7da4ffeeadcfd3273800ab02bf766f992bc2b4
altinselimi <altinselimi@msn.com>
2017-07-18 23:52:27 +0200

    scrolling fixed

--- test/specs/flatelect.spec.js
+++ test/specs/flatelect.spec.js
@@ -4,7 +4,7 @@ import { createVM } from '../helpers/utils.js'
 describe('flatelect.vue', function () {
   it('should render correct contents', function () {
     const vm = createVM(this, `
-<flatelect :options="['Alaska', 'Arizona', 'Vushtrri']"></flatelect>
+<flatelect :options="['Alaska', 'Arizona', 'Vushtrri', 'Airport', 'Prishtine', 'Ericsonville', 'New Jersey', 'Europe', 'Asia']"></flatelect>
 `, { components: { flatelect }})
     vm.$el.querySelector('.select-filter').hasChildNodes().should.eql(true)
   })
